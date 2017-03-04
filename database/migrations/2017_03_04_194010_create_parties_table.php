7b9eca42ac3e6c604457ade654f164627d9ab528
Kyle Eisenbarger <keisenb@ksu.edu>
2017-03-04 14:09:53 -0600

    Setup migrations

---
+++ database/migrations/2017_03_04_194010_create_parties_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('parties', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->string('password')->nullable();
+          $table->integer('host')->unsigned();
+          $table->integer('threshold')->unsigned()->default(5);
+          $table->integer('created_by')->unsigned();
+          $table->integer('queue')->default(0);
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('parties');
+
+    }
+}
