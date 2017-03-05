5134208cb1616e59ed0f0c6d7d4655c095489955
Kyle Eisenbarger <keisenb@ksu.edu>
2017-03-05 10:49:43 -0600

    Fixed merge conflicts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.master')
 @section('title', 'Home')
 @section('content')
-<!-- Panel (Banner) -->
   <section class="panel banner right">
     <div class="content color0 span-3-75">
       <h1 class="major">Welcome to<br />
       DJCollab</h1>
-      <p>This is <strong>Ethereal</strong>, a free site template by AJ for <a href="https://html5up.net">HTML5 UP</a>. It’s fully responsive, built on HTML5 and CSS3, and released entirely for free under the Creative Commons license. Hope you dig it :)</p>
+      <p>This is <strong>DJCollab</strong>, a web and mobile application built so that you and your friends can queue up some 🔥🔥🔥 beats.</p>
       <ul class="actions">
         <li><a href="#first" class="button special color1 circle icon fa-angle-right">Next</a></li>
       </ul>
@@ -16,38 +15,47 @@
     </div>
   </section>
 
-<!-- Panel (Spotlight) -->
   <section class="panel spotlight medium right" id="first">
     <div class="content span-7">
-      <h2 class="major">Sed etiam aenean</h2>
-      <p>Mauris et ligula arcu. Proin dapibus convallis accumsan. Lorem maximus hendrerit orci, sit amet elementum massa hendrerit sed. Donec et ullamcorper ligula. Suspendisse amet potenti. Ut pretium libero eleifend euismod sed tristique. Quisque dictum magna risus, id ultricies justo sagittis vitae. Sed id odio tempor, porttitor elit amet, gravida hendrerit fringilla lorem ipsum dolor.</p>
+      <h2 class="major">Host a Party</h2>
+      <p>Become the ultimate DJ.</p>
+      <ul class="actions">
+        <li><a href="{{ url('/dashboard') }}" class="button special color1 circle icon fa-angle-right">Login</a></li>
+      </ul>
     </div>
     <div class="image filtered tinted" data-position="top left">
       <img src="{{ URL::asset('img/2.jpg') }}" alt="" />
     </div>
   </section>
 
-<!-- Panel -->
-  <section class="panel color1">
-    <div class="intro joined">
-      <h2 class="major">Amet lorem</h2>
-      <p>Sed vel nibh libero. Mauris et lorem pharetra massa lorem turpis congue pulvinar. Vivamus sed feugiat finibus. Duis amet bibendum amet sed. Duis mauris ex, dapibus sed ligula tempus volutpat magna etiam.</p>
-    </div>
-    <div class="inner">
-      <ul class="grid-icons three connected">
-        <li><span class="icon fa-diamond"><span class="label">Lorem</span></span></li>
-        <li><span class="icon fa-camera-retro"><span class="label">Ipsum</span></span></li>
-        <li><span class="icon fa-cog"><span class="label">Dolor</span></span></li>
-        <li><span class="icon fa-paper-plane"><span class="label">Sit</span></span></li>
-        <li><span class="icon fa-bar-chart"><span class="label">Amet</span></span></li>
-        <li><span class="icon fa-code"><span class="label">Nullam</span></span></li>
+  <section class="panel spotlight large left">
+    <div class="content span-5">
+      <h2 class="major">Join a Party</h2>
+      <p>Start queuing up your favorite songs.</p>
+      <ul class="actions">
+        <li><a href="{{ url('/dashboard') }}" class="button special color1 circle icon fa-angle-right">Login</a></li>
       </ul>
+    </div>
+    <div class="image filtered tinted" data-position="top right">
+      <img src="{{ URL::asset('img/3.jpg') }}" alt="" />
     </div>
   </section>
 
-
-
-<!-- Copyright -->
-  <div class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</div>
-
+  <section class="panel color4-alt">
+    <div class="intro color4">
+      <h2 class="major">Contact</h2>
+      <p>Have a question? Want to more ways to make some fire beats? Hit us up!</p>
+    </div>
+    <div class="inner columns divided">
+      <div class="span-1-5">
+        <ul class="contact-icons color1">
+          <li class="icon fa-twitter"><a href="#">@djcollab</a></li>
+          <li class="icon fa-facebook"><a href="#">facebook.com/djcollab</a></li>
+          <li class="icon fa-snapchat-ghost"><a href="#">@djcollab</a></li>
+          <li class="icon fa-instagram"><a href="#">@djcollab</a></li>
+          <li class="icon fa-medium"><a href="#">medium.com/djcollab</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
 @endsection
